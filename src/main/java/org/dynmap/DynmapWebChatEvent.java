1ce3db491ea2597f0467c6fc7ac1f2160507130a
Mike Primm <mike@primmhome.com>
2012-02-11 21:57:46 -0600

    Update events for 1.1 event system (breaks 1.0 compatibility)

--- src/main/java/org/dynmap/DynmapWebChatEvent.java
+++ src/main/java/org/dynmap/DynmapWebChatEvent.java
@@ -1,14 +1,17 @@
 package org.dynmap;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
 
 /**
  * Custom Bukkit event, corresponding to the receiving of a web-chat message from a web UI user
  * 
  * This interface is Bukkit specific.
  */
+@SuppressWarnings("serial")
 public class DynmapWebChatEvent extends Event implements Cancellable {
     public static final String CUSTOM_TYPE = "org.dynmap.DynmapWebChatEvent";
+    private static final HandlerList handlers = new HandlerList();
     private String source;
     private String name;
     private String message;
@@ -37,4 +40,12 @@ public class DynmapWebChatEvent extends Event implements Cancellable {
     public boolean isProcessed() { return isprocessed; }
     
     public void setProcessed() { isprocessed = true; }
+    
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
 }
