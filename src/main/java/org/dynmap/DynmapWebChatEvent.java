e856060bcffbfd20894c7daac52d4e7c59bfce2e
Mike Primm <mike@primmhome.com>
2014-04-21 19:48:08 -0500

    A bit of cleanup

--- src/main/java/org/dynmap/DynmapWebChatEvent.java
+++ src/main/java/org/dynmap/DynmapWebChatEvent.java
@@ -25,8 +25,10 @@ public class DynmapWebChatEvent extends Event implements Cancellable {
         this.cancelled = false;
         this.isprocessed = false;
     }
+    @Override
     public boolean isCancelled() { return cancelled; }
     
+    @Override
     public void setCancelled(boolean cancel) { cancelled = cancel; }
 
     public String getSource() { return source; }
